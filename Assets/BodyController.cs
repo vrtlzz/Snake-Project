4ba44fdd26b90973700358f39beb8e966e75a3fb
vrtlzz <italoazevedo92@gmail.com>
2023-12-06 07:26:33 +0100

    Added a score text
    Added a score text to the game, and food will spam always in the camera range.

--- Assets/BodyController.cs
+++ Assets/BodyController.cs
@@ -1,16 +1,22 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BodyController : MonoBehaviour
 {
-    
+    public Text text;
+    int score;
     public Transform Food;
     public Transform Body;
     public List<Transform> bodyList = new List<Transform>();
+    Camera cam;
 
     private void Start()
     {
         bodyList.Add(this.transform);
+
+         cam = Camera.main;
+        score = 0;
     }
 
 
@@ -20,20 +26,30 @@ public class BodyController : MonoBehaviour
             for (int i = bodyList.Count - 1; i > 0; i--)
             {
                 bodyList[i].transform.position = bodyList[i - 1].transform.position;
-            } 
+            }
+
+        text.text = ("Score: " + score);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Food"))
         {
-            Vector3 randomPosition = new Vector3(
-                                     Random.Range(-40, 40),
-                                     Random.Range(-19, 19), 0);
+
+
+            Vector3 randomViewportPoint = new Vector3( Random.value, Random.value, 0f);
+
+            
+            Vector3 randomWorldPoint = cam.ViewportToWorldPoint(randomViewportPoint);
+
+            randomWorldPoint.z = 0f;
 
             Destroy(collision.gameObject);
-            Instantiate(Food, randomPosition, transform.rotation);
+
+            Instantiate(Food, randomWorldPoint, transform.rotation);
+
             Grow();
+            score++;
 
         }
     }
